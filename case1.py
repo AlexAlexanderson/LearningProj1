6dcfed013f0bea9699cd7b48f5244ba9cbca8676
Alex_Tropin <alexanderson.tir@gmail.com>
2023-01-09 12:43:54 +0300

    кейс 1 итерация 1 фикс 3

--- case1.py
+++ case1.py
@@ -1,64 +1,16 @@
-import unicodedata
-
-# со stackoverflow: удаляем управляющие символы
-def remove_control_characters(s):
-
-    return "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
-
-# преобразуем строку в число если возможно
-def str_to_int_if_possible(value):
-
-    value_is_float = False
-
-    for s in value:
-        if (s == "-" or s == '.' or s.isdigit()) != True:
-            return value
-        if s == '.':
-            value_is_float = True
-    if value_is_float:
-        return float(value)
-    else:
-        return int(value)
-
-# преобразуем строку в словарь если возможно
-def str_to_dict_if_possible(str_data):
-
-    dict_data = dict()
-
-    # удалим либо заменим неподходящие символы
-    str_data = remove_control_characters(str_data)
-    str_data = str_data.replace("  ", " ")
-    str_data = str_data.replace("\"", "")
-    str_data = str_data.replace("\'", "")
-    str_data = str_data.replace(":", ",")
-    str_data = str_data.replace(";", ",")
-
-    # со stackoverflow: превращаем строку в словарь
-    # по красоте не подходит т.к. с неподходящей строкой валится в ошибку
-    # dict_data = dict((a.strip(), str_to_int_if_possible(b.strip()))
-    #                  for a, b in (element.split('=')
-    #                               for element in str_data.split(', ')))
-
-    # свое поделие для превращения строки в словарь
-    list_of_dict_el = str_data.split(',')
-    for dict_el in list_of_dict_el:
-        key_and_value = dict_el.strip().split("=")
-        if len(key_and_value) == 2:
-            dict_data[key_and_value[0].strip()] = str_to_int_if_possible(key_and_value[1].strip())
-
-    if len(dict_data) == 0:
-        print("unable to get values from string")
-        return None
-    else:
-        return dict_data
-
-
-# собственно, функция кейса
-def case1_func(string_data):
-
-    dict_data = str_to_dict_if_possible(string_data)
-    if dict_data == None:
-        return
+def case1_func(**dict_data):
+    """
+    ТЗ Кейса:
+    Написать функцию, которая принимает на вход сколько угодно параметров,
+    находит среди этих параметров, все которые начинаются со слова "sum".
+    На выход возвращает все найденные параметры, а также сумму значений этих параметров.
+    Требования к решению:
+    Вашу функцию нельзя сломать из вне. Передаю ей всё что угодно, она не вызывает ошибок.
+    А лишь выводит уведомление о том что она не понимает что я от неё хочу.
+    Бороться с ошибками в данном кейсе нужно используя только условные конструкции (без try).
+    :param dict_data:
+    :return:
+    """
 
     result = 0
     for key, value in dict_data.items():
--- main.py
+++ main.py
@@ -1,5 +1,66 @@
 import case1
+import unicodedata
+
+def remove_control_characters(s):
+    """
+    со stackoverflow: удаление управляющих символов
+    :param s:
+    :return:
+    """
+    return "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
+
+def str_to_int_if_possible(value):
+    """
+    преобразование строки в число если возможно
+    :param value:
+    :return:
+    """
+
+    value_is_float = False
+
+    for s in value:
+        if (s == "-" or s == '.' or s.isdigit()) != True:
+            return value
+        if s == '.':
+            value_is_float = True
+    if value_is_float:
+        return float(value)
+    else:
+        return int(value)
+
+def str_to_dict_if_possible(str_data):
+    """
+    преобразование строки в словарь если возможно
+    :param str_data:
+    :return:
+    """
+    dict_data = dict()
+
+    # удалим либо заменим неподходящие символы
+    str_data = remove_control_characters(str_data)
+    str_data = str_data.replace("  ", " ")
+    str_data = str_data.replace("\"", "")
+    str_data = str_data.replace("\'", "")
+    str_data = str_data.replace(":", ",")
+    str_data = str_data.replace(";", ",")
+
+    list_of_dict_el = str_data.split(',')
+    for dict_el in list_of_dict_el:
+        key_and_value = dict_el.strip().split("=")
+        if len(key_and_value) == 2:
+            dict_data[key_and_value[0].strip()] = str_to_int_if_possible(key_and_value[1].strip())
+
+    if len(dict_data) == 0:
+        print("unable to get values from string")
+        return None
+    else:
+        return dict_data
 
 str_data = input('input arguments (example: aaa=1, sum1=1, sum2=2.1, bbb="bbb", sum3=100, sum4="one hundred"): ')
+
+dict_data = str_to_dict_if_possible(string_data)
+    if dict_data == None:
+        return
+
 case1.case1_func(str_data)
 
